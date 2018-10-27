3e77fcd5ee3b2a0038b6ddc54204f3733d1f8130
Fazendaaa <lucas.carotta@outlook.com>
2018-10-27 01:22:15 -0300

    Missing only occurrences.

--- R/manipulacao.R
+++ R/manipulacao.R
@@ -18,74 +18,9 @@
 calcularTotal <- function(dados) {
     total <- dados %>%
                 group_by(Bloco) %>%
-                summarise(total = n())
+                summarise(Total = n())
 
     return (total)
-}
-
-#' Calcular Posição
-#'
-#' @description
-#' Pega o dados da tabela e calcula as medidas de posição como:
-#' \itemize{
-#'     \item 
-#'     \item 
-#'     \item 
-#'}
-#'
-#' @author Fazendaaa
-#'
-#' @param dados Data frame para realizar tarefa
-#'
-#' @return Dada frame com os dados calculados
-#'
-#' @keywords internal
-#'
-calcularPosicao <- function(dados) {
-
-}
-
-#' Calcular Dispersão
-#'
-#' @description
-#' Pega o dados da tabela e calcula as medidas de disperção como:
-#' \itemize{
-#'     \item 
-#'     \item 
-#'     \item 
-#'}
-#'
-#' @author Fazendaaa
-#'
-#' @param dados Data frame para realizar tarefa
-#'
-#' @return Dada frame com os dados calculados
-#'
-#' @keywords internal
-#'
-calcularDispersao <- function(dados) {
-
-}
-
-#' Calcular Tamanho
-#'
-#' @description
-#' Pega o dados da tabela e calcula medidas de posição e disperção
-#'
-#' @author Fazendaaa
-#'
-#' @param dados Data frame para realizar tarefa
-#'
-#' @return Dada frame com os dados calculados
-#'
-#' @export
-#'
-calcularMedidas <- function(dados) {
-    # posicao <- cacularPosicao(dados)
-    # dispercao <- cacularDispersao(dados)
-    # medidas <- injetar as duas anteriores
-
-    # return (medidas)
 }
 
 #' Calcular Infiltrações
@@ -110,7 +45,7 @@ calcularInfiltracoes <- function(dados) {
     total <- dados %>%
                 filter(Infiltr == 1) %>%
                 group_by(Bloco) %>%
-                summarise(Infiltr = n())
+                summarise(Infiltracao = n())
 
     return (total)
 }
--- R/principal.R
+++ R/principal.R
@@ -8,13 +8,36 @@ NULL
 
 tabela <- lerArquivo()
 
+ggplot(tabela, aes(x = Bloco, y = Andar)) + geom_boxplot()
+ggsave('./data/Andar.png')
+
+ggplot(tabela, aes(x = Bloco, y = Final)) + geom_boxplot()
+ggsave('./data/Final.png')
+
+ggplot(tabela, aes(x = Bloco, y = Sala)) + geom_boxplot()
+ggsave('./data/Sala.png')
+
+ggplot(tabela, aes(x = Bloco, y = Cozinha)) + geom_boxplot()
+ggsave('./data/Cozinha.png')
+
+ggplot(tabela, aes(x = Bloco, y = Banheiro)) + geom_boxplot()
+ggsave('./data/Banheiro.png')
+
+ggplot(tabela, aes(x = Bloco, y = Dorm)) + geom_boxplot()
+ggsave('./data/Dorm.png')
+
 total <- tabela %>% calcularTotal
-infiltracoes <- tabela %>% calcularInfiltracoes
+ggplot(data = total, aes(x = Bloco, y = Total)) + geom_bar(stat = 'identity')
+ggsave('./data/Total.png')
+
 rachaduras <- tabela %>% calcularRachaduras
+ggplot(data = rachaduras, aes(x = Bloco, y = Rachadura)) + geom_bar(stat = 'identity')
+ggsave('./data/Rachadura.png')
+
+infiltracoes <- tabela %>% calcularInfiltracoes
+ggplot(data = infiltracoes, aes(x = Bloco, y = Infiltracao)) + geom_bar(stat = 'identity')
+ggsave('./data/Infiltracao.png')
 
 dados <- list(total, infiltracoes, rachaduras)
-
 processados <- Reduce(function(x, y) merge(x, y, all = TRUE), dados)
-
-paraVisualizar <- as.data.frame(processados)
-print(paraVisualizar)
+print(processados)
